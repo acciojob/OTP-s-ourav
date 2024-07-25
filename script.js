c6a97f9c869ca9eb8f6912b8df7159dbd7367c50
Sourav Mazumdar <sourav.mozu@gmail.com>
2024-07-25 14:19:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-//your JS code here. If required.
-inputs=document.getElementsByClassName("code")
+inputs=Array.from(document.getElementsByClassName("code"))
+// console.log(inputs)
 inputs.forEach((inp,idx)=>{
 	inp.addEventListener(("keyup"),(event)=>{
+        console.log(event);
 		if( event.keyCode >= 48 && event.keyCode <= 57 && idx+1<inputs.length){
 			inputs[idx+1].focus()
 		}
 		else if (event.keyCode===8 && idx-1>=0){
+			inp.value=''
 			inputs[idx-1].focus()
 		}
 	})
+	console.log(inp);
 })
